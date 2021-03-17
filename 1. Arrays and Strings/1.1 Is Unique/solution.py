311c67422df91fd1e97cac93c111ec070e94d2c0
ztherebe <49122551+ztherebe-github@users.noreply.github.com>
2021-03-17 16:09:22 +0800

    Update solution.py

--- 1. Arrays and Strings/1.1 Is Unique/solution.py
+++ 1. Arrays and Strings/1.1 Is Unique/solution.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def isUnique(self, astr: str) -> bool:
+        l=len(astr)
+        a=""
+        for i in range(l):
+            if astr[i] not in a:
+                a=a+astr[i]
+            else:
+                return False
+        return True
