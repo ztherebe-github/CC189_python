9f393720540f223e20503fb0d2fd726d279349f7
ztherebe <49122551+ztherebe-github@users.noreply.github.com>
2021-03-17 18:04:54 +0800

    Delete 1. Arrays and Strings/1.2 Check Permutation directory

--- 1. Arrays and Strings/1.2 Check Permutation/CheckPermutation.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def CheckPermutation(self, s1: str, s2: str) -> bool:
-        if len(s1) != len(s2):
-            return False
-        else:
-            for i in s1:
-                if s1.count(i)!=s2.count(i):
-                    return False
-            return True
